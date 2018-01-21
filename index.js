3b6eda6b5019a408928aeaf9a662b928beabbec5
gosha-24@yandex.ru <gosha-24@yandex.ru>
2018-01-21 18:22:05 +0300

    completed with async/await

--- index.js
+++ index.js
@@ -1,73 +1,57 @@
 const fs = require("fs");
 const path = require("path");
+const util = require('util');
+
+const readdir = util.promisify(fs.readdir);
+const stat = util.promisify(fs.stat);
+const mkdir = util.promisify(fs.mkdir);
+const copyFile = util.promisify(fs.copyFile);
+const unlink = util.promisify(fs.unlink);
+const rmdir = util.promisify(fs.rmdir);
 
 let srcDir = process.argv[2];
 let destinationDir = process.argv[3];
 let removeSrc = !!process.argv[4];
 
-let copyFile = (from, to, remove) => {
+let performDir = async (srcPath, destPath, remove) => {
 
-    fs.copyFile(from,to, (err) => { 
-        if (err) {    
-            console.log("ERROR COPYING!" + err);
-            throw err;
+    try { 
+      let files = await readdir(srcPath)
+      for (let file of files) {
+        let stats = await stat(path.join(srcPath, file));
+        if (stats.isDirectory()) {
+          await performDir(path.join(srcPath, file), destPath, remove);
+        } else { //IF FILE:
+          let from = path.join(srcPath, file);
+          let destDir = path.join(destPath, file.charAt(0));
+          let to = path.join(destDir, file);
+          
+          try {
+            let destStats = await stat(destDir)
+          }
+          catch (err) {
+            if (err.code === "ENOENT") {
+              console.log('CREATE DIR:' + destDir);
+              await mkdir(destDir);
+            } else {
+              console.log('ERROR READING DIRECTORY: ' + destDir);
+              throw err;
+            }
+          }
+
+          await copyFile(from, to);
+          if (remove)
+            await unlink(from);
+
         }
-        if (remove) {
-            fs.unlink(from, (err) => {
-                if (err)
-                    throw err;
-            })
-        }});
-}
-
-let performDir = (srcPath, distPath, removeSrc) => {
-
-    fs.readdir(srcPath, (err, files) => {
-
-        if (err) {
-            console.log('ERROR READING DIRECTORY ' + srcPath + ': ' + err.message);
-            throw err;
-        }
-        files.forEach( (item) => {
-
-            fs.stat(path.join(srcPath, item), (err, stats) => {
-
-                if (err) {
-                    console.log('ERROR while getting stats for ' + path.join(srcPath, item) + ': ' + err.message);
-                    throw err;
-                }
-                if (stats.isDirectory()) {
-                    console.log('DIRECTORY FOUND: ' + path.join(srcPath, item));
-                    performDir(path.join(srcPath, item), distPath, removeSrc);
-                }
-                else {// if file
-                    console.log('NEED TO MOVE FILE ' + item + ' TO ' + path.join(distPath, item.charAt(0)));
-                    fs.stat(path.join(distPath, item.charAt(0)), function(err, stats) {
-                        let from = path.join(srcPath, item);
-                        let destDir = path.join(distPath, item.charAt(0));
-                        let to = path.join(destDir, item);
-                        if (err && err.code === "ENOENT") {
-                            console.log('NO DIRECTORY ' + destDir + ' FOUND, LETS CREATE IT'); 
-                            fs.mkdir(destDir, (err) => {
-                                if (err) {
-                                    if (err.code != 'EEXIST') { // directory maybe exists, maybe created concurrently.
-                                        console.log('UNABLE TO CREATE DIRECTORY ' + destDir + ': ' + err.code);
-                                        throw err;
-                                    }
-                                }
-                                copyFile(from,to, removeSrc);
-                                
-                            })
-                        } else {
-                            copyFile(from,to, removeSrc);
-                        
-                        }
-                    });
-                }});
-
-        });
-
-    });
+      };
+      console.log('READING FINISHED: ' + srcPath);
+      if (remove)
+        await rmdir(srcPath);
+    } catch (err) {
+      console.log(err.message);
+      throw err;
+    }
 }
 
 performDir(srcDir, destinationDir, removeSrc);
